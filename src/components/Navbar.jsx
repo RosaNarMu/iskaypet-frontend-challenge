54d8de0ea906a029899df3b80597c3ed1e8abbb2
RosaNM <rosanmn.n@gmail.com>
2022-04-07 19:05:30 +0200

    navbar scroll in process

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+
+export default function Navbar() {
+
+
+
+    return (
+
+        <>
+
+            <div className="navbar-container">
+                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            </div>
+
+        </>
+
+    )
+}
--- src/views/ToDoList.jsx
+++ src/views/ToDoList.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import Header from '../components/Header'
+import Navbar from '../components/Navbar'
 
 export default function ToDoList() {
     return (
-
-        <Header />
-
+        <>
+            <Header />
+            <Navbar />
+        </>
     )
 }
