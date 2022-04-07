2d128e383347c330a9a7b2db982d120d32938a57
RosaNM <rosanmn.n@gmail.com>
2022-04-07 15:13:16 +0200

    header implemented

--- src/TestFrontend.jsx
+++ src/TestFrontend.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ToDoList from './views/ToDoList'
 
 export const TestFrontend = () => {
     return (
         <div>
-            <h1>Mis tareas</h1>
+            <ToDoList />
         </div>
     )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+export default function Header() {
+    return (
+        <header>
+            <img src="/assets/Menu.png" alt="Menu icon" />
+            <img src="/assets/Search.png" alt="Search icon" />
+            <img src="/assets/Logo.png" alt="Logo icon" />
+            <img src="/assets/Login.png" alt="Login icon" />
+            <img src="/assets/Cart.png" alt="Cart icon" />
+        </header>
+    )
+}
