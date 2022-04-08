26fec75e3b1d488a41d0bc6539d3e11508f4a7f1
RosaNM <rosanmn.n@gmail.com>
2022-04-08 14:34:34 +0200

    box shadow implemented with horizontal scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,16 @@ export default function Navbar() {
 
 
     return (
+        <nav className="navbar">
+            <ul className="navbar-container">
+                <li>Mis datos</li>
+                <li className="active">Mis tareas</li>
+                <li>Mis devoluciones</li>
+                <li>Mis comunicaciones</li>
+                <li>Mis mejores amigos</li>
+            </ul>
 
-        <ul className="navbar-container">
-            <li>Mis datos</li>
-            <li className="active">Mis tareas</li>
-            <li>Mis devoluciones</li>
-            <li>Mis comunicaciones</li>
-            <li>Mis mejores amigos</li>
-        </ul>
+        </nav>
 
     )
 }
