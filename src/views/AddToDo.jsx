3af59d04168a3a6cc19eaaf1aa80ba845cc723aa
RosaNM <rosanmn.n@gmail.com>
2022-04-10 14:53:18 +0200

    mobile first style completed

--- src/views/AddToDo.jsx
+++ src/views/AddToDo.jsx
@@ -5,8 +5,24 @@ export default function AddToDo({ togglePopup }) {
     return (
         <div className='popup-box'>
             <div className='box'>
-                <button className='btn-close' onClick={togglePopup}>x</button>
-                <span>popup content</span>
+                <button className='btn-close' onClick={togglePopup}><img src="/assets/close-icon.png" alt="Close icon" /></button>
+
+                <span className="add-todo-title">Añadir tarea</span>
+
+                <form /* onSubmit={} */ className='add-todo-form'>
+
+                    <label className='add-todo-label'>Nombre</label>
+                    <input className='add-todo-input' type="text" placeholder='Nombre' />
+
+                    <label className='add-todo-label'>Descripción</label>
+                    <textarea className='add-todo-input description' type="text" placeholder='Descripción' />
+
+                    <div className='add-todo-form-btn-container'>
+                        <button className='add-todo-form-btn-cancel'>Cancelar</button>
+                        <button className='add-todo-form-btn-add'>Añadir</button>
+                    </div>
+
+                </form>
 
             </div>
         </div >
